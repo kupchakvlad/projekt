c7343e99ff89be676c274614d1f7438e361afa11
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-10-10 18:57:44 +0200

    удалил хуйню и написал коментарии в пхп файл

--- code.php
+++ code.php
@@ -10,19 +10,19 @@ $password_registration = password_hash($_POST["password_registration"], PASSWORD
 //writes data to database
 $registration_data = "$username_registration  |  $email_registration  |  $password_registration\n";
 
+//writes data on click and redirect to the main page if data have been written well
 if (isset($_POST["register"])){
     $database = fopen("registration_data.txt", "a");
     fwrite($database, $registration_data);
     fclose($database);
+    header("Location: main.html");
+    exit();
 }
 
+//redirects to login_form.html
 if (isset($_POST["login"])){
     header("Location: login_form.html");
     exit();
 }
-
-//redirects user to main page after registration. change file name to redirect user to another file
-header("Location: main.html");
-exit();
 
 ?>
