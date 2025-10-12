457cf9a3dc9c7b2a7276454689b42e4c5e02cd6d
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-10-12 16:21:57 +0200

    сделал папки подсоеденил все по новой

--- code.php
+++ code.php
@@ -1,6 +1,6 @@
 <?php
 
-$REGISTRATION_DATABASE = "registration_data.txt";
+$REGISTRATION_DATABASE = "registration_database.txt";
 
 //registration data from registration_form.html to database form -> tag(name)
 $username_registration = $_POST["username_registration"];
@@ -12,22 +12,21 @@ $registration_data = "$username_registration  |  $email_registration  |  $passwo
 
 //writes data on click and redirect to the main page if data have been written well
 if (isset($_POST["register_submit"])){
-    $database = fopen("registration_data.txt", "a");
+    $database = fopen($REGISTRATION_DATABASE, "a");
     fwrite($database, $registration_data);
     fclose($database);
     header("Location: main.html");
     exit();
 }
 
-//redirects to login_form.html ----- works if <formnovalidate> atribute is written in login button tag
+//redirects to login_form.html from registration_form.html ----- works if <formnovalidate> atribute is written in login button tag
 if (isset($_POST["login"])){
-    header("Location: login_form.html");
+    header("Location: login_form/login_form.html");
     exit();
 }
-
 //redirects to registration_form.html from login_form.html if the button is clicked ---- works if <formnovalidate> atribute is written in register button tag
 if (isset($_POST["register"])){
-    header("Location: registration_form.html");
+    header("Location: registration_form/registration_form.html");
     exit();
 }
 
