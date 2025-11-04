a45595798c05bc916594cffeadc72e0bc645520e
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-11-04 11:55:44 +0100

    added event listeners to the registration form and removed onclick atribute from registration_form.html

--- registration_form/registration_form.js
+++ registration_form/registration_form.js
@@ -4,19 +4,22 @@ const registration_form = document.getElementById("registration_form");
 const login_form = document.getElementById("login_form");
 const darkMode = document.getElementById("dark-mode-btn");
 
-function showLogin(){
+
+login_button.addEventListener("click", () => {
     login_button.classList.add("active");
     login_form.classList.add("active");
     registration_button.classList.remove("active");
     registration_form.classList.remove("active");
-}
+});
 
-function showRegistration(){
+
+registration_button.addEventListener("click", () => {
     registration_button.classList.add("active");
     registration_form.classList.add("active");
     login_button.classList.remove("active");
     login_form.classList.remove("active");
-}
+});
+
 
 darkMode.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode")
