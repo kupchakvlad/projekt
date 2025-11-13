0a9b7776b81b497fb615eba115551cf455f5c7e5
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-11-13 21:47:30 +0100

    small changes

--- add_product/add_product.js
+++ add_product/add_product.js
@@ -3,13 +3,12 @@ const output = document.getElementById("size-value");
 const darkModeBtn = document.getElementById("dark-mode-btn");
 const BackBtn = document.querySelector(".back-to-main")
 
-function shoeSize() {
-    output.textContent = slider.value;
 
-    slider.addEventListener("input", () => {
-        output.textContent = slider.value;
-    });
-}
+output.textContent = slider.value;
+slider.addEventListener("input", () => {
+    output.textContent = slider.value;
+});
+
 
 darkModeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
