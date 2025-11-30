8106079a477f287d50b606b05ba135edc1037145
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-11-30 23:17:15 +0100

    proba16

--- registration_form/registration_form.php
+++ registration_form/registration_form.php
@@ -1,18 +1,6 @@
 <?php
 session_start();
 
-//DATABASE
-$host = "localhost";
-$username = "kupchvla";
-$password = "webove aplikace";
-$database = "kupchvla";
-
-$connection = mysqli_connect($host, $username, $password, $database);
-
-if (!$connection) {
-    die("Connect failed: \n". mysqli_connect_error());
-}
-
 //REGISTRATION VALIDATION
 $errorMessages = [];
 
