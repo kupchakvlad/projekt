3367126090a84b8f0633abf8f21bc3b8a24cbac9
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-12-01 18:25:01 +0100

    ispravil js v registration_form.js

--- registration_form/registration_form.js
+++ registration_form/registration_form.js
@@ -1,6 +1,46 @@
-let byPassListener = false;
+const darkMode = document.getElementById("dark-mode-btn");
 
-registration_form.addEventListener("submit", function(event) {
+const registrationForm = document.getElementById("registration_form");
+const loginForm = document.getElementById("login_form");
+
+const registrationBtn = document.getElementById("registration_button");
+const loginBtn = document.getElementById("login_button");
+
+const registration_password = document.getElementById("registration_password");
+const registration_password_confirmation = document.getElementById("registration_password_confirmation");
+const registration_email = document.getElementById("registration_email");
+const registration_email_container = document.getElementById("registration_email_container");
+const registration_password_container = document.getElementById("registration_password_container");
+
+
+darkMode.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+});
+
+registrationBtn.addEventListener("click", function() {
+    registrationForm.classList.remove("active");
+    loginForm.classList.add("active");
+
+    registrationBtn.classList.remove("active");
+    loginBtn.classList.add("active");
+});
+
+loginBtnBtn.addEventListener("click", function() {
+    registrationForm.classList.add("active");
+    loginForm.classList.remove("active");
+
+    registrationBtn.classList.add("active");
+    loginBtn.classList.remove("active");
+});
+
+
+let byPassListener = false;
+let emailMessage;
+let passwordMessage_8;
+let passwordMessage_confirmation;
+let passwordStrengthMessage;
+
+registrationForm.addEventListener("submit", function(event) {
     if (byPassListener) return; // prevent recursion
 
     event.preventDefault();
@@ -96,15 +136,14 @@ registration_form.addEventListener("submit", function(event) {
                 registration_password_container.appendChild(passwordStrengthMessage);
             }
         } else {
-            // Password ok → submit form
             registration_password.classList.remove("password_error");
             if (passwordStrengthMessage) {
                 passwordStrengthMessage.remove();
                 passwordStrengthMessage = null;
             }
 
-            byPassListener = true; // prevent recursion
-            registration_form.submit();
+            byPassListener = true;
+            registrationForm.submit();
         }
     };
     request.send();
