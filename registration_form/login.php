53214247eb14ddbbc2c5ab01ca90ccf750bdbabe
Ansar Rinatuly - SourceTree <rinatans@cvut.cz>
2025-12-04 21:44:21 +0100

    Сделал login.php и добавил при ошибочном ведении красные линии и тд в css. Осталось registration.html переименовать в registration.php и исправить js

--- registration_form/login.php
+++ registration_form/login.php
@@ -1,6 +1,50 @@
 <?php
+session_start();
 
 
+$host = "localhost";
+$username = "kupchvla";
+$password = "webove aplikace";
+$database = "kupchvla";
+
+$connection = mysqli_connect($host, $username, $password, $database);
+if (!$connection) {
+    die("Connect failed: " . mysqli_connect_error());
+}
 
 
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["login_email"])) {
+    $login_email = trim($_POST["login_email"]);
+    $login_password = trim($_POST["login_password"]);
+    $error_message = "";
+
+    if (empty($login_email) || empty($login_password)) {
+        $error_message = "Email and password are required";
+
+    } else{
+        $query = "SELECT id, password FROM users WHERE email = ?";
+        $stmt = mysqli_prepare($connection, $query);
+        mysqli_stmt_bind_param($stmt, "s", $login_email);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $user_id, $hashed_password);
+        mysqli_stmt_fetch($stmt);
+        mysqli_stmt_close($stmt);
+
+        if ($user_id) {
+            if (password_verify($login_password, $hashed_password)) {
+                $_SESSION["user_id"] = $user_id;
+                header("Location: ../main/main.php");
+                exit;
+            } else {
+                $error_message = "Incorrect password";
+            }
+        } else {
+            $error_message = "User with this email doesn't exist";
+
+        }
+    }
+    $_SESSION['login_error'] = $error_message;
+    header("Location: registration_form.html");
+    exit;
+}
 ?>
