3d3b8e1cccca1d8277beb324759672296c43215c
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-12-07 01:55:16 +0100

    dobavil cookie na dark mode 34

--- account/account.js
+++ account/account.js
@@ -1,12 +1,10 @@
 // Dark mode toggle
 const darkModeToggle = document.getElementById('dark-mode-toggle');
-if (darkModeToggle) {
-    darkModeToggle.addEventListener('click', function() {
-        document.body.classList.toggle("dark");
-        const isDark = document.body.classList.toggle("dark");
-        document.cookie = `mode=${isDark ? 'dark' : 'light'}; path=/;`;
-    });
-}
+darkModeToggle.addEventListener('click', function() {
+    document.body.classList.toggle("dark");
+    const isDark = document.body.classList.toggle("dark");
+    document.cookie = `mode=${isDark ? 'dark' : 'light'}; path=/;`;
+});
 
 // Back button
 const backButton = document.getElementById('back-button');
