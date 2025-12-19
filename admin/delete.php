0fcbaa90f898132d53db258a93debfa995097726
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-12-19 22:35:18 +0100

    pathhh

--- admin/delete.php
+++ admin/delete.php
@@ -17,7 +17,7 @@ $stmt = mysqli_prepare($connection, $delete_user_query);
 mysqli_stmt_bind_param($stmt, "i", $user_id);
 
 if (mysqli_stmt_execute($stmt)) {
-    header("Location: ~kupchvla/projekt/admin/admin.php");
+    header("Location: admin.php");
     exit;
 } else {
     echo "User cannot be deleted";
