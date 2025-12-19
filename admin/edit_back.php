1da2bac17bb6e09eb1d747e1be275c2d652793ac
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-12-19 23:45:59 +0100

    edit

---
+++ admin/edit_back.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+$host = "localhost";
+$username = "kupchvla";
+$password = "webove aplikace";
+$database = "kupchvla";
+
+$connection = mysqli_connect($host, $username, $password, $database);
+
+if (!$connection) {
+    die("Connection failed: \n". mysqli_connect_error());
+}
+
+if (isset($_POST['edit']) && !empty($_POST['edited_username']) && !empty($_POST['edited_email'])) {
+    $query = "UPDATE users SET name = ?, email = ? WHERE id = ?";
+    $stmt = mysqli_prepare($connection, $query);
+    mysqli_stmt_bind_param($stmt, "ssi", $_POST['edited_username'], $_POST['edited_email'], $_GET['id']);
+    mysqli_stmt_execute($stmt);
+
+    header("Location: admin.php");
+    exit;
+}
+?>
