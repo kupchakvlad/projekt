9d54a7f1e587845596e09bf1240cb53cbbffc196
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-12-19 23:47:58 +0100

    edit

--- admin/edit_back.php
+++ admin/edit_back.php
@@ -12,13 +12,4 @@ if (!$connection) {
     die("Connection failed: \n". mysqli_connect_error());
 }
 
-if (isset($_POST['edit']) && !empty($_POST['edited_username']) && !empty($_POST['edited_email'])) {
-    $query = "UPDATE users SET name = ?, email = ? WHERE id = ?";
-    $stmt = mysqli_prepare($connection, $query);
-    mysqli_stmt_bind_param($stmt, "ssi", $_POST['edited_username'], $_POST['edited_email'], $_GET['id']);
-    mysqli_stmt_execute($stmt);
-
-    header("Location: admin.php");
-    exit;
-}
 ?>
