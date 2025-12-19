c63227aa36614fd09693afc2ef0abd5b8a555930
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-12-19 17:44:15 +0100

    admin page

---
+++ admin/admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>(Admin mode)</title>
+    <link href="admin.css" rel="stylesheet">
+</head>
+<body>
+
+<table>
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Password</th>
+    </tr>
+    </thead>
+    <tbody class="users">
+
+    </tbody>
+</table>
+
+</body>
+</html>
