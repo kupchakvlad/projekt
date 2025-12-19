2f18599918300ed92f99456ba6495b60780e54d3
Ansar Rinatuly - SourceTree <rinatans@cvut.cz>
2025-12-19 20:48:15 +0100

    добавление товаров на мэйн страницу после загружение на старнице add product

--- main/main.php
+++ main/main.php
@@ -8,6 +8,19 @@ if (!isset($_SESSION["user_id"])) {
 }
 
 $dark_mode_class = (isset($_COOKIE['mode']) && $_COOKIE['mode'] === 'dark') ? 'dark' : '';
+
+$host = "localhost";
+$username = "kupchvla";
+$password = "webove aplikace";
+$database = "kupchvla";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+if (!$conn) {
+    die("DB error");
+}
+
+$query = "SELECT * FROM products ORDER BY id DESC";
+$result = mysqli_query($conn, $query);
 ?>
 
 
@@ -71,7 +84,34 @@ $dark_mode_class = (isset($_COOKIE['mode']) && $_COOKIE['mode'] === 'dark') ? 'd
 
 
   <section class="products" id="products">
-    <!-- Здесь будут товары -->
+    <?php
+$query = "
+SELECT * 
+FROM products
+WHERE id IN (
+    SELECT MIN(id)
+    FROM products
+    GROUP BY name
+)
+ORDER BY id DESC
+";
+
+$result = mysqli_query($conn, $query);
+
+if ($result) {
+    while ($product = mysqli_fetch_array($result)) {
+        echo '<div class="product-card">';
+        echo '<img src="' . str_replace('/home/kupchvla/www', 'https://zwa.toad.cz/~kupchvla', $product['file_path']) . '" alt="product">';
+        echo '<p class="product-name">' . $product["name"] . '</p>';
+        echo '<p class="product-brand">' . $product["fabric"] . '</p>';
+        echo '<p> Season:' . $product["season"] . '</p>';
+        echo '<p> Size:' . $product["size"] . '</p>';
+        echo '<p class="price"> Price:' . $product["price"] . ' CZK </p>';
+        echo '</div>';
+    }
+}
+?>
+
   </section>
 </main>
 
