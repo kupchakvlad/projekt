70af6cd0d3aca91b19f99cf3193bd4f9d6aeabbe
Ansar Rinatuly - SourceTree <rinatans@cvut.cz>
2025-12-19 23:48:05 +0100

    первый скелет страницы продукта с хуевым css

--- main/product.php
+++ main/product.php
@@ -32,3 +32,30 @@ $product = mysqli_fetch_array($result);
 if (!$product) {
     die("This product does not exit");
 } 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?php echo $product["name"]; ?> - Botovo </title>
+    <link rel="stylesheet" href="product.css">
+</head>
+<body class="<?php echo $dark_mode_class; ?>">
+<div class = "product-page">
+
+    <img src="<?php echo str_replace('/home/kupchvla/www', 'https://zwa.toad.cz/~kupchvla', $product['file_path']); ?>" alt="product">
+    <h1><?php echo $product['name']; ?></h1>
+    <p class="brand"><?php echo $product['fabric']; ?></p>
+    <p>Season: <?php echo $product['season']; ?></p>
+    <p>Size: <?php echo $product['size']; ?></p>
+    <p class="price"><?php echo $product['price']; ?> CZK</p>
+    <a href="main.php">Back to main</a>
+
+
+</div>
+    
+</body>
+</html>
