d532258efa1663854abd7c0c9424171a426d87ca
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-12-20 20:51:13 +0100

    fixim

--- admin/edit.php
+++ admin/edit.php
@@ -21,7 +21,7 @@ $user_id = $_GET['id'];
 </head>
 
 <body>
-    <form method="POST">
+    <form method="POST" action="edit_back.php?id=<?php echo $user_id; ?>">
         <label>
             Username:
             <input type="text" name="edited_username" required>
