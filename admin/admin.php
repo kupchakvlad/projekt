03e111a629b1f06231a1cde2b4c8c8060862934f
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-12-21 00:30:23 +0100

    fix8

--- admin/admin.php
+++ admin/admin.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$_SESSION["admin_status"] = "true";
+$_SESSION["admin"] = "true";
 
 if (!isset($_SESSION['user_id']) || !$_SESSION['admin'] || $_SESSION["admin"] != -1) {
     header("Location: ../registration_form/registration_form.php");
