f9af92193b05e04e5be0640df3c61a4e280f7465
Ansar Rinatuly - SourceTree <rinatans@cvut.cz>
2025-12-27 19:01:24 +0100

    Удалил комменты

--- account/account.js
+++ account/account.js
@@ -1,4 +1,3 @@
-// Dark mode toggle
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 darkModeToggle.addEventListener('click', function() {
     const isDark = document.body.classList.toggle("dark");
@@ -9,7 +8,6 @@ darkModeToggle.addEventListener('click', function() {
     request.send(`mode=${newMode}`);
 });
 
-// Back button
 const backButton = document.getElementById('back-button');
 backButton.addEventListener('click', function() {
     window.location.href = "../main/main.php";
--- set_dark_mode_cookie.php
+++ set_dark_mode_cookie.php
@@ -1,6 +1,4 @@
 <?php
-// dlja js
-// v js v peremennoj data, mode=`${data}`, ${data} - peredajotsja
 $new_mode = $_POST["mode"];
 
 setcookie("mode", $new_mode, 0, '/');
