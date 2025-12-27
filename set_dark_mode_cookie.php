5a2d40b10666bdda90ff6435633907454e89b433
Vladyslav Kupchak <kupchvla@student.cvut.cz>
2025-12-27 20:32:52 +0100

    dobavil perechod na registration form jestli prochodit srazu na set dark mode cookies

--- set_dark_mode_cookie.php
+++ set_dark_mode_cookie.php
@@ -1,4 +1,10 @@
 <?php
+if (!isset($_SESSION["user_id"])) {
+    header("Location: ../registration_form/registration_form.php");
+    exit;
+}
+
+
 $new_mode = $_POST["mode"];
 
 setcookie("mode", $new_mode, 0, '/');
